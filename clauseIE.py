a4a02a2296cf4666a15ae11604658163ba8a1150
drwiner <drwiner131@gmail.com>
2017-07-04 14:10:25 -0600

    added text processing tools

--- clauseIE.py
+++ clauseIE.py
@@ -1,5 +1,19 @@
-class A():
-	def __init__(self):
-		pass
-        
-    
+
+DO_PRINT = 0
+
+def Log(message):
+	if DO_PRINT:
+		print(message)
+
+def loadSpacy():
+	import spacy
+	print('loading SPACY english')
+	nlp = spacy.load('en')
+	return nlp
+
+
+if __name__ == '__main__':
+	# command line args (TODO)
+	# steps for processing text
+
+	nlp = loadSpacy()
